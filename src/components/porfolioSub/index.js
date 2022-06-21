a83176b9024eeff06d9825c3e450ea409a1af64b
Hiram Moises Lugo Rodriguez <hiram1397@gmail.com>
2022-06-21 12:36:37 -1100

    third commit

--- src/components/porfolioSub/index.js
+++ src/components/porfolioSub/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 import profolio1 from '../../images/fotosdelportafolio/trasportation.jpg'
 import profolio2 from '../../images/fotosdelportafolio/singlelogo.JPG'
 import profolio3 from '../../images/fotosdelportafolio/39fcf9_7ff2fcd89c9f4d35baed1db4db848d89_mv2.png'
-import profolio4 from '../../images/fotosdelportafolio/servicios.jpg'
+import profolio4 from '../../images/fotosdelportafolio/justice.jpg'
 
-const PortfolioSub = ( props ) => {
-    return(
+const PortfolioSub = (props) => {
+    return (
         <div className={`wpo-protfolio-area section-padding ${props.subclass}`}>
             <div className="container">
                 <div className="col-12">
@@ -24,19 +24,19 @@ const PortfolioSub = ( props ) => {
                                     <div className="wpo-protfolio-left">
                                         <div className="wpo-protfolio-single">
                                             <div className="wpo-protfolio-img">
-                                                <img src={profolio1} alt=""/>
+                                                <img src={profolio1} alt="" />
                                             </div>
                                             <div className="wpo-protfolio-text">
-                                                
+
                                                 <Link to="/projectDetails">View Work</Link>
                                             </div>
                                         </div>
                                         <div className="wpo-protfolio-single">
                                             <div className="wpo-protfolio-img">
-                                                <img src={profolio2} alt=""/>
+                                                <img src={profolio2} alt="" />
                                             </div>
                                             <div className="wpo-protfolio-text">
-                                               
+
                                                 <Link to="/projectDetails/1">View Work</Link>
                                             </div>
                                         </div>
@@ -46,19 +46,19 @@ const PortfolioSub = ( props ) => {
                                     <div className="wpo-protfolio-right">
                                         <div className="wpo-protfolio-single">
                                             <div className="wpo-protfolio-img">
-                                                <img src={profolio3} alt=""/>
+                                                <img src={profolio3} alt="" />
                                             </div>
                                             <div className="wpo-protfolio-text">
-                                               
+
                                                 <Link to="/projectDetails/2">View Work</Link>
                                             </div>
                                         </div>
                                         <div className="wpo-protfolio-single">
                                             <div className="wpo-protfolio-img">
-                                                <img src={profolio4} alt=""/>
+                                                <img src={profolio4} alt="" />
                                             </div>
                                             <div className="wpo-protfolio-text">
-                                                
+
                                                 <Link to="/projectDetails/3">View Work</Link>
                                             </div>
                                         </div>
